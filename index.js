251d31263138345b05377ebb3561edfa346ae4a8
Zzza38 <155106336+Zzza38@users.noreply.github.com>
2024-10-13 12:25:37 -0400

    check

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import packageJson from './package.json' assert { type: 'json' };
 
 // Set up path resolution
 const __dirname = path.resolve();
-//const uvPath = path.join(__dirname, '/proxy/'); // Ensure the 'proxy' directory exists
+const uvPath = path.join(__dirname, '/proxy/'); // Ensure the 'proxy' directory exists
 const server = http.createServer();
 const bareServer = createBareServer('/bear/');
 const app = express(server);
